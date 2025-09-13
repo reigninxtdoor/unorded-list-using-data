06c939c7ce524ee00d0bc800666a642942fdae76
nazir martin <155297402+reigninxtdoor@users.noreply.github.com>
2025-09-13 13:03:25 +0000

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import FiltersList from "./Filters/index";
 //TODO: import the data model for your list of filters
-
+import filterData from "./data";
 const App = () => (
   <div className="App">
     {/* TODO: pass your data model as a prop to your FiltersList component */}
-    <FiltersList />
+    <FiltersList filters={filterData} />
   </div>
 );
 
--- src/data.js
+++ src/data.js
@@ -1,1 +1,49 @@
 //TODO: add and export the data model for your list of filters
+const filterData = [
+  {
+    id: 21,
+    name: "Free cancellation",
+    matchcount: 307,
+  },
+  {
+    id: 22,
+    name: "Breakfast included",
+    matchcount: 175,
+  },
+  {
+    id: 23,
+    name: "Best sellers",
+    matchcount: 20,
+  },
+  {
+    id: 41,
+    name: "parking",
+    matchcount: 272,
+  },
+  {
+    id: 42,
+    name: "pets-allowed",
+    matchcount: 164,
+  },
+  {
+    id: 43,
+    name: "room-services",
+    matchcount: 170,
+  },
+  {
+    id: 44,
+    name: "fitness center",
+    matchcount: 249,
+  },
+  {
+    id: 45,
+    name: "restaurants",
+    matchcount: 350,
+  },
+  {
+    id: 46,
+    name: "tourist attractions",
+    matchcount: 21,
+  },
+];
+export default filterData;
